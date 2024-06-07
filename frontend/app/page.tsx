6d0eb65578c257beb8efa232994842b6fdbceae2
Sion Kim <94289633+mandlake@users.noreply.github.com>
2024-06-07 16:06:38 +0900

    update all

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,23 +10,23 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center h-screen w-screen">
         <div className="flex flex-col gap-[0.5vh]">
           <div className="text-[var(--color-Harbor-firth)] bg-[var(--color-Harbor-first)] flex flex-col justify-center items-center align-middle w-[41.53vw] h-[18.85vh]">
-            <h1 className="font-normal text-[9.77vh] my-[4.54vh]">Lawmate</h1>
+            <h1 className="font-normal text-[6.948vw] m-[4.539vh]">Lawmate</h1>
           </div>
           <div className=" justify-start flex flex-col gap-[0.5vh]">
-            <div className="bg-[var(--color-Harbor-firth)] w-[20vw] hover:bg-gradient-to-r from-[var(--color-Harbor-first)] to-[var(--color-Harbor-firth)] transition duration-500 ease-in-out px-[2vw]">
+            <div className="bg-[var(--color-Harbor-firth)] w-[41.53vw] hover:bg-gradient-to-r from-[var(--color-Harbor-first)] to-[var(--color-Harbor-firth)] transition duration-500 ease-in-out px-[2vw]">
               <button
                 onClick={() => router.push(`/pages/users/join`)}
                 className="text-[var(--color-Harbor-second)] hover:text-[var(--color-Harbor-firth)] transition duration-500 ease-in-out"
               >
-                회원가입으로 이동 -&gt;
+                회원가입 -&gt;
               </button>
             </div>
-            <div className="bg-[var(--color-Harbor-firth)] w-[20vw] hover:bg-gradient-to-r from-[var(--color-Harbor-first)] to-[var(--color-Harbor-firth)] transition duration-500 ease-in-out px-[2vw]">
+            <div className="bg-[var(--color-Harbor-firth)] w-[41.53vw] hover:bg-gradient-to-r from-[var(--color-Harbor-first)] to-[var(--color-Harbor-firth)] transition duration-500 ease-in-out px-[2vw]">
               <button
                 onClick={() => router.push(`/pages/users/login`)}
                 className="text-[var(--color-Harbor-second)] hover:text-[var(--color-Harbor-firth)] transition duration-500 ease-in-out"
               >
-                로그인으로 이동 -&gt;
+                로그인 -&gt;
               </button>
             </div>
           </div>
